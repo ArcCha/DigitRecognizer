685fc1e25c0f1ca27a1fa68f20eb5078ce057d9f
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2018-05-05 00:05:12 +0200

    Add statistic gathering

--- net.py
+++ net.py
@@ -2,9 +2,9 @@ from torch import nn
 from torch.nn import functional as F
 
 
-class Net(nn.Module):
+class SimpleCNN(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
+        super(SimpleCNN, self).__init__()
         self.conv1 = nn.Conv2d(1, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
--- train.py
+++ train.py
@@ -1,15 +1,22 @@
+import json
+import time
+from pathlib import Path
+
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
-from net import Net
+from net import *
 from PIL import Image
 from torch.autograd import Variable
 from torchvision import transforms
+from tqdm import tqdm
+
+torch.manual_seed(48)
 
 train_path = '/home/arccha/.kaggle/competitions/digit-recognizer/train.csv'
-DATA_NUM = 42000  # 42000 - max
+DATA_NUM = 1000  # 42000 - max
 data = np.genfromtxt(train_path, delimiter=',',
                      skip_header=1, max_rows=DATA_NUM)
 
@@ -22,15 +29,24 @@ transform = transforms.Compose([
 X = list(map(Image.fromarray, X))
 X = list(map(transform, X))
 
-
-net = Net()
+H = {}  # Training history and statistics
+net = SimpleCNN()
+H['net'] = type(net).__name__
+net_dir = Path('./' + H['net'])
+net_dir.mkdir(parents=True, exist_ok=True)
 
 optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
+H['optimizer'] = str(optimizer)
 criterion = nn.CrossEntropyLoss()
+H['criterion'] = str(criterion)
 
-for epoch in range(10):
+EPOCH_NUM = 10
+H['epoch_num'] = EPOCH_NUM
+H['loss'] = []
+start = time.process_time()
+for epoch in tqdm(range(EPOCH_NUM), desc='Total'):
     running_loss = 0.0
-    for i, x in enumerate(X):
+    for i, x in enumerate(tqdm(X, desc='Epoch ' + str(epoch))):
         x = x.unsqueeze(0)
         y = torch.LongTensor(Y[i])
         optimizer.zero_grad()
@@ -39,10 +55,21 @@ for epoch in range(10):
         loss.backward()
         optimizer.step()
 
-        running_loss += loss.data[0]
-        if i % 100 == 99:
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
+        running_loss += loss.item()
+        if i == 0:
+            H['initial_loss'] = loss.item()
+        #print("[{}, {}]: {}".format(epoch, i, loss.item()))
+    H['loss'].append(running_loss / DATA_NUM)
+    #print("Epoch {}: {}".format(epoch, running_loss / DATA_NUM))
 
-torch.save(net.state_dict(), './net_state')
+
+end = time.process_time()
+H['learning_duration'] = end - start
+net_state_path = net_dir.joinpath('net.state')
+net_state_path.touch(exist_ok=True)
+with net_state_path.open(mode='wb') as f:
+    torch.save(net.state_dict(), f)
+net_stats_path = net_dir.joinpath('stats.json')
+net_stats_path.touch(exist_ok=True)
+with net_stats_path.open('w') as f:
+    json.dump(H, f, indent=2, sort_keys=True)
