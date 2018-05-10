8a40fd7154125f88c189c44e33f10e1f4c8b62df
byebye <jakub.jan.labaj@gmail.com>
2018-05-10 22:17:30 +0200

    Plot confusion matrix

--- train.py
+++ train.py
@@ -1,19 +1,15 @@
 import json
 import time
-from pathlib import Path
 
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torchvision
 import yaml
+from sklearn.metrics import confusion_matrix
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
 from cuda import *
 from data import *
 from net import *
-from torch.autograd import Variable
-from torch.utils.data import DataLoader
-from tqdm import tqdm
+from plot import plot_confusion_matrix
 
 config_path = Path('train_config.yaml')
 config = None
@@ -59,7 +55,17 @@ H['loss'] = []
 H['train_acc'] = []
 H['test_acc'] = []
 start = time.process_time()
+
+predicted_train = []
+true_train = []
+predicted_test = []
+true_test = []
+
 for epoch in tqdm(range(config['epoch_num']), desc='Total'):
+
+    def is_last_epoch():
+        return epoch + 1 == config['epoch_num']
+
     running_loss = 0.0
     for x, y in tqdm(train_loader, desc='Epoch ' + str(epoch)):
         x = x.to(device)
@@ -78,6 +84,10 @@ for epoch in tqdm(range(config['epoch_num']), desc='Total'):
         y_true = y_true.to(device)
         y_pred = net(x).argmax(dim=1)
         acc += y_true.eq(y_pred).sum()
+        if is_last_epoch():
+            true_train += y_true.numpy().tolist()
+            predicted_train += y_pred.numpy().tolist()
+
     acc = float(acc) / (len(train_loader) * config['batch_size'])
     H['train_acc'].append(acc)
     acc = 0
@@ -86,6 +96,10 @@ for epoch in tqdm(range(config['epoch_num']), desc='Total'):
         y_true = y_true.to(device)
         y_pred = net(x).argmax(dim=1)
         acc += y_true.eq(y_pred).sum()
+        if is_last_epoch():
+            true_test += y_true.numpy().tolist()
+            predicted_test += y_pred.numpy().tolist()
+
     acc = float(acc) / config['validation_num']
     H['test_acc'].append(acc)
     lr_scheduler.step(acc)
@@ -109,3 +123,11 @@ net_stats_path = net_dir.joinpath('stats.json')
 net_stats_path.touch(exist_ok=True)
 with net_stats_path.open('w') as f:
     json.dump(H, f, indent=2, sort_keys=True)
+
+cnf_matrix = confusion_matrix(true_train, predicted_train)
+plot_confusion_matrix(cm=cnf_matrix, classes=list(range(10)),
+                      title='Confusion matrix, without normalization')
+
+cnf_matrix = confusion_matrix(true_test, predicted_test)
+plot_confusion_matrix(cm=cnf_matrix, classes=list(range(10)),
+                      title='Confusion matrix, without normalization')
